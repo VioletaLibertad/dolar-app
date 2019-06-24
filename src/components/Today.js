85705f9cc473827c6f2fe53c98e2b2ba7a282883
Violeta Libertad <morada.libertad@gmail.com>
2019-06-24 19:47:21 -0400

    structure components added

---
+++ src/components/Today.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Today() {
+  return (
+    <div className="today-container">
+      <h1>Dolar-App</h1>
+      <h3>Tu fuente de información confiable</h3>
+      <h4>El valor del dólar para hoy es:</h4>
+      <h4>666.6</h4>
+    </div>
+  );
+}
+
+export default Today;
