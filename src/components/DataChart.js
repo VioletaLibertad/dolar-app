49478e16f7f081ab1f3b0d983e773c5c50d21f53
Violeta Libertad <morada.libertad@gmail.com>
2019-06-26 11:35:59 -0400

    default chart added

--- src/components/DataChart.js
+++ src/components/DataChart.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { LineChart, Line, XAxis, CartesianGrid, Tooltip } from 'recharts';
 
-function DataChart() {
-  return (
-    <div className="today-container">
-      <h3>El gráfico bien maravilloso</h3>
-    </div>
-  );
+const data = [
+  {name: 'Page A', uv: 4000, pv: 4400, amt: 2400},
+  {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
+  {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
+  {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+  {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
+  {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
+  {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+];
+
+class DataChart extends React.Component {
+  render() {
+    return (
+      <div className="chart-container">
+        <LineChart width={600} height={300} data={data} margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+          <XAxis dataKey="name"/>
+          <CartesianGrid strokeDasharray="3 3"/>
+          <Tooltip/>
+          <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}/>
+        </LineChart>
+      </div>
+    );
+  }
 }
 
 export default DataChart;
