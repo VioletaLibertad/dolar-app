c1de7d48e9b507c7c55d46a6cdff5ac56c674079
Violeta Libertad <morada.libertad@gmail.com>
2019-06-26 22:24:18 -0400

    Updates

---
+++ precache-manifest.50daec746f2603f0d2a4558a4738c215.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0be466693c71ba5aa888cc68625bf035",
+    "url": "/dolar-app/index.html"
+  },
+  {
+    "revision": "f212ae83d241bc132fd9",
+    "url": "/dolar-app/static/css/2.c7bb94bc.chunk.css"
+  },
+  {
+    "revision": "f212ae83d241bc132fd9",
+    "url": "/dolar-app/static/js/2.30eba6bb.chunk.js"
+  },
+  {
+    "revision": "4ca922697beb9faa9cae",
+    "url": "/dolar-app/static/js/main.08be30f2.chunk.js"
+  },
+  {
+    "revision": "c098524fd33e169f3e50",
+    "url": "/dolar-app/static/js/runtime~main.e4af46e2.js"
+  }
+]);
